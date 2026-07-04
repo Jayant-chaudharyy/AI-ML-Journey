4a2732bf5130e901e912450975e71dbd3e885275
Jayant chaudharyy <chaudharyjayant270@gmail.com>
2026-07-04 19:01:16 +0530

    Day 2 -practice the integer and sol 15 questions

---
+++ Python-Basics/Day1-basics of python/int-practice-sol.py
@@ -0,0 +1,70 @@
+# QUESTION -1
+base_price = 100
+discount = 20
+total_price = base_price - discount
+print("The total price is : ", total_price)
+# or
+print(f"The total price is : {total_price}")
+
+# QUESTION -2
+print(5**2)
+
+# QUESTION -3
+print(10//3)
+
+# QUESTION -4
+num = 17
+if num % 2 == 0:
+    print("yes its a even")
+else:
+    print("it an odd")
+
+# QUESTION -5
+a = 10
+b = 20
+
+a, b = b, a
+print(f"a = {a} and b = {b}")
+
+# QUESTION -6
+x = 20
+result = (x+5)*2-3
+print(result)
+
+# QUESTION -7
+flt_num = 15.9
+int_num = int(flt_num)
+print(int_num)
+
+# QUESTION -8
+day = 1
+day += 1
+print(day)
+
+# QUESTION -9
+print(abs(-50))
+
+# QUESTION -10
+hours = 2
+seconds = hours * 60 * 60
+print(seconds)
+
+# QUESTION -11
+int_1 = 20
+int_2 = 10
+print(int_1 > int_2)
+
+# QUESTION -12
+total_requests = 1050
+div = int(total_requests / 100)
+print(div)
+
+# QUESTION -13
+print(2**10)
+
+# QUESTION -14
+print(100 % 7)
+
+# QUESTION -15
+x_1 = 5
+print(x_1 > 3 and x_1<10)
