b2734ba6c35fd80d87571967da85f156c4ad0dd3
Jayant-chaudharyy <chaudharyjayant270@gmail.com>
2026-07-09 20:03:34 +0530

    day4-dictionary-concept-understanding and working

--- Python-Basics/day4-dictionary/introduction.py
+++ Python-Basics/day4-dictionary/introduction.py
@@ -5,7 +5,6 @@ dict_example = {"name": "jayant"}
 print(type(dict_example), dict_example)
 
 # In this key should be unique and not repeating and its value can be any datatype like float, boolean, integer, string etc.
-
 # A dictionary representing an AI model configuration
 model_profile = {
     "name": "gemini-2.5-flash-lite",  # string value
@@ -14,7 +13,7 @@ model_profile = {
     "supported_languages": ["Python", "TypeScript", "Go"]  # list
 }
 
-
+#------------------------------------------------------------------------------------------------------------
 # Operation on Dictionary :
 # ---Accesing value ---:
 #  variable_name["key_name"] --- syntax
@@ -22,23 +21,33 @@ print(model_profile["version"])
 # to check whether key exist or not use .get("key_name")
 print(model_profile.get("is_active"))
 
-
+#---------------------------------------------------------------------------------------------------------
 # ---Adding/Updating----:
 #  variable_name["key_name"]="value_item" --- syntax
 model_profile["Paid"] = "yes"
 print(model_profile)
 
-
+#----------------------------------------------------------------------------------------------------------
 # ---Removing/Delete---:
 # To Delete entire key:value pair --- del variable_name["key_name"]
 del model_profile["Paid"]
 print(model_profile)
 
-# To delete the key and return its value --- variable_name.pop("key_name")
-model_profile.pop("is_active")
-print(model_profile)
+# To delete the key and return its value --- variable_name.pop("key_name") 
+#to make variable out of dictionary while deleting its key
+user_profile = {
+    "username": "jayant_dev",
+    "status": "active"
+}
+
+current_status = user_profile.pop("status")
+ # new_variable_name = dictionary_varaible_name.pop("key_name")
+ 
+print(user_profile)    # Output: {'username': 'jayant_dev'} (The status pair is deleted!)
+print(current_status)  # Output: active (The value is safely saved here!)
 
 
+#-----------------------------------------------------------------------------------------------------------
 # ---Extract----
 # extract the list of keys
 print(model_profile.keys())
