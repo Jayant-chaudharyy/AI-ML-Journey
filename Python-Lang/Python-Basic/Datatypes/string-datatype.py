df141f6965c01adad82402cc4ecb7c8a9b6e7313
Jayant-chaudharyy <chaudharyjayant270@gmail.com>
2026-09-17 17:35:31 +0530

    Python-Basics__Day-2-strings-Datatype

--- Python-Lang/Python-Basic/Datatypes/string-datatype.py
+++ Python-Lang/Python-Basic/Datatypes/string-datatype.py
@@ -1,1 +1,87 @@
-#question -1 
+# # question -1
+
+# name = "jayant chaudhary"
+# print(f"My name is {name}", type(name))
+
+# # question -2
+# name = "Jayant"
+# course = "MCA AI/ML"
+# university = "Bennett university"
+
+# print(f"Name: {name}")
+# print(f"Course: {course}")
+# print(f"University: {university}")
+
+# # question -3
+# first_name = "Jayant"
+# last_name = "Chaudhary"
+# full_name = first_name + " " + last_name
+# print(full_name)
+
+# # question -4
+# word = "python"
+# print(word*3)
+
+# # question -5
+# a = str(input("Enter the text here: "))
+# print(f"The user string is {a}")
+# print("The length of string is ", len(a))
+
+# # question -6
+# text = "python"
+# print(text[0])
+# print(text[1])
+# print(text[2])
+# print(text[3])
+# print(text[4])
+# print(text[5])
+
+# # question -7
+# text = "PROGRAMMING"
+# print(text[-1])
+# print(text[-2])
+# print(text[-3])
+
+# # question -8
+# text = "PYTHON"
+# print(text[0])
+# print(text[2])
+# print(text[-1])
+# print(text[-2])
+
+# # question -9
+# text = "PROGRAMMING"
+# print(text[:4])
+# print(text[-4:])
+# print(text[2:7])
+
+# # question -10
+# str_a = str(input("ENTER THE TEXT :"))
+# print(str_a[::-1])
+
+# # question -11
+# b = str(input("Enter the text: "))
+# print(b)
+# print(b.upper())
+# print(b.lower())
+# print(b.capitalize())
+# print(b.swapcase())
+
+# # question -12
+# text = "   Python is Amazing   "
+# print(text.strip())
+
+# # question -13
+# text = "I love Java"
+# print(text.replace("Java", "Python"))
+
+# # Question -14
+# text = "Python is easy to learn and Python is powerful"
+# print(text.find("Python"))
+# print(text.find("powerful"))
+
+# # question -15
+# a = str(input("Enter the text: "))
+# b = str(input("Enter the character: "))
+# print(a.count(b))
+
