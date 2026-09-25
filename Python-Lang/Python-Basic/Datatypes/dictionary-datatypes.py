336968b38680d973392c5f61a8374ab56b4bdced
Jayant-chaudharyy <chaudharyjayant270@gmail.com>
2026-09-25 16:10:47 +0530

    Python-Basics__Day-5-Dictionary-Datatype-completed

--- Python-Lang/Python-Basic/Datatypes/dictionary-datatypes.py
+++ Python-Lang/Python-Basic/Datatypes/dictionary-datatypes.py
@@ -104,7 +104,104 @@
 # marks.clear()
 # print(marks)
 
-# question -11 
-student_marks = {
-    "Python" : input("Enter the python: ")
+# # question -11
+# student_marks = {
+#     "Python" : float(input("Enter the python: ")),
+#     "SQL" : float(input("Enter the SQL: ")),
+#     "Math" : float(input("Enter the Math: ")),
+#     "AI" : float(input("Enter the AI: ")),
+#     "ML" : float(input("Enter the ML: "))
+# }
+# marks = student_marks.values()
+# total_marks = sum(marks)
+# print("Total Marks ",total_marks)
+# avg_marks = total_marks /5
+# print("Average Marks ",avg_marks)
+# print("Higest Marks ", max(marks))
+# print("Higest Marks ", min(marks))
+
+# # question -12
+# student = {
+#     "name": "Jayant",
+#     "course": "MCA",
+#     "skills": ["Python", "SQL", "Git"]
+# }
+# student["skills"].extend(["Pandas", "Numpy"])
+# print("Skills :", student["skills"])
+# print(student)
+
+# # quuestion -13
+# student_skills = {
+#     input("Enter the skills-1: "),
+#     input("Enter the Skills-2: "),
+#     input("Enter the skills-3: "),
+#     input("Enter the skills-4: "),
+#     input("Enter the skills-5: ")
+# }
+
+# student = {
+#     "name" : input("enter the name: "),
+#     "course" : input("enter the course: "),
+#     "skills" : student_skills
+# }
+
+# print(student)
+# student_skills.pop()
+# print(student)
+# print("Python exist :", "Python" in student_skills)
+# print(student)
+
+# # question -14
+# students = {
+#     "student1": {
+#         "name": "Jayant",
+#         "age": 23,
+#         "course": "MCA"
+#     },
+#     "student2": {
+#         "name": "Rahul",
+#         "age": 22,
+#         "course": "BCA"
+#     }
+# }
+
+# print(students["student1"]["name"])
+# print(students["student1"]["course"])
+# print(students["student2"]["name"])
+# print(students["student2"]["course"])
+
+# students["student2"]["course"] = "MCA"
+# print(students)
+
+# question -15
+student_skills = {
+    input("Enter the skills-1: "),
+    input("Enter the Skills-2: "),
+    input("Enter the skills-3: "),
+    input("Enter the skills-4: "),
+    input("Enter the skills-5: ")
 }
+student = {
+    "name": input("enter the name: "),
+    "age": int(input("enter the age: ")),
+    "mail": input("enter Email: "),
+    "course": input("enter the course: "),
+    "university": input("enter the university: "),
+    "city": input("enter the city: "),
+    "skills": student_skills
+}
+
+print(student)
+print(student.keys())
+print(student.values())
+print(student.items())
+print(student["name"])
+print(student["skills"])
+student_skills.add("Rust")
+print("Added Skills: ", student_skills)
+student_skills.discard("Rust")
+print("Remove skills :", student_skills)
+print("Python exist", "Python" in student_skills)
+student["city"] = "meerut"
+student["key"] = "0"
+print(student)
